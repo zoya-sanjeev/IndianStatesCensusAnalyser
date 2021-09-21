71303d0b66a33230c0473dc085700e1667225eef
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-09-21 21:06:57 +0530

    ADDED: CensusAnalyserException class

--- lib/src/main/java/com/bridgelabz/indianstatecensusanalyser/CensusAnalyserException.java
+++ lib/src/main/java/com/bridgelabz/indianstatecensusanalyser/CensusAnalyserException.java
@@ -1,5 +1,17 @@
 package com.bridgelabz.indianstatecensusanalyser;
 
-public class CensusAnalyserException {
-
+public class CensusAnalyserException extends Exception{
+	
+	enum ExceptionType{
+		CENSUS_FILE_PROBLEM,
+		UNABLE_TO_PARSE,
+		CENSUS_INCORRECT_FILE_FORMAT
+	}
+	
+	ExceptionType type;
+	
+	 public CensusAnalyserException(String message, ExceptionType type) {
+		 super(message);
+	     this.type = type;
+	   } 
 }
