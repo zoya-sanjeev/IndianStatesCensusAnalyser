a2ad2fe387a54cd0480e055c78704d8401b7022c
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-09-21 17:38:40 +0530

    ADDED: function to load census data

--- lib/src/main/java/com/bridgelabz/indianstatecensusanalyser/StateCensusAnalyser.java
+++ lib/src/main/java/com/bridgelabz/indianstatecensusanalyser/StateCensusAnalyser.java
@@ -1,5 +1,36 @@
 package com.bridgelabz.indianstatecensusanalyser;
 
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.util.Iterator;
 public class StateCensusAnalyser {
+	
+	public int loadIndiaCensusData(String csvFilePath) {
+		
+		int numberOfEntries = 0;
+		try {
+			Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
+			CsvToBeanBuilder<CSVStateCensus> csvToBeanBuilder=new CsvToBeanBuilder<>(reader);
+			csvToBeanBuilder.withType(CSVStateCensus.class);
+			csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+			CsvToBean<CSVStateCensus> csvToBean = csvToBeanBuilder.build();
+			Iterator<CSVStateCensus> censusCSVIterator=csvToBean.iterator();
+	
+    		while(censusCSVIterator.hasNext()) {
+    			numberOfEntries++;
+    			CSVStateCensus censusData = censusCSVIterator.next();
+    		}
+    		
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return numberOfEntries;
+	}
 
 }
