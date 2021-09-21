c45f12f7aa2efada6ff39d5bbd2a472a678990bb
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-09-21 16:06:50 +0530

    ADDED: IndianStatesCensusCSV class created

--- lib/src/main/java/com/bridgelabz/indianstatecensusanalyser/IndianStatesCensusCSV.java
+++ lib/src/main/java/com/bridgelabz/indianstatecensusanalyser/IndianStatesCensusCSV.java
@@ -3,6 +3,29 @@
  */
 package com.bridgelabz.indianstatecensusanalyser;
 
+import com.opencsv.bean.CsvBindByName;
+
 public class IndianStatesCensusCSV {
+	@CsvBindByName(column = "State", required = true)
+    public String state;
+
+    @CsvBindByName(column = "Population", required = true)
+    public int population;
+
+    @CsvBindByName(column = "AreaInSqKm", required = true)
+    public int areaInSqKm;
+
+    @CsvBindByName(column = "DensityPerSqKm", required = true)
+    public int densityPerSqKm;
+
+    @Override
+    public String toString() {
+        return "IndiaCensusCSV{" +
+                "State='" + state + '\'' +
+                ", Population='" + population + '\'' +
+                ", AreaInSqKm='" + areaInSqKm + '\'' +
+                ", DensityPerSqKm='" + densityPerSqKm + '\'' +
+                '}';
+    }
 
 }
